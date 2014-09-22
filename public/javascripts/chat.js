7fe7df591a28b310c69425b243489fa2d6cbfe62
App Academy Student <student@appacademy.io>
2014-09-22 15:18:58 -0700

    can change nickname and detects logging in and logging out

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,7 +1,26 @@
 function Chat(socket) {
   this.socket = socket;
 }
+
+Chat.prototype.processCommand = function (command) {
+  var split = command.split(' ');
+  var cmd = split[0].substr(1);
+  var args = split.slice(1);
+
+  switch (cmd) {
+    case 'nick':
+      var nickname = args[0];
+      this.socket.emit('nicknameChangeRequest', { 
+        nickname: nickname 
+      });
+      break;
+    
+    default:
+      console.log('Command:', cmd, 'Arguments:', args);
+      break;
+  }
+};
  
-Chat.prototype.sendMessage = function(message){
-  socket.emit('message', { message: message });
+Chat.prototype.sendMessage = function (message) {
+  this.socket.emit('message', { message: message });
 };
--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -1,10 +1,11 @@
 var socket = io();
 
-function ChatUI($form, $input, $messages) {
+function ChatUI($form, $input, $messages, $users) {
   this.running = false;
   this.$form = $form;
   this.$input = $input;
   this.$messages = $messages;
+  this.$users = $users;
   this.chat = new Chat(socket);
 }
 
@@ -21,27 +22,91 @@ ChatUI.prototype.bindEvents = function () {
   this.$form.on('submit', function (event) {
     event.preventDefault();
     
-    var message = this.getMessage();
-    this.sendMessage(message);
+    var input = this.getInputValue();
+    
+    if (input[0] === '/') {
+      this.chat.processCommand(input);
+    } else {
+      this.chat.sendMessage(input); 
+    }
+    
+    this.resetForm();
   }.bind(this));
   
+  socket.on('listUsers', function(data){
+    data.users.forEach( this.addUser.bind(this));
+  }.bind(this));
+  
+  socket.on('userConnect', function (data) {
+    this.recvMessage(data.nickname + " has connected.");
+    this.addUser(data.nickname);
+  }.bind(this));
+  
+  socket.on('userDisconnect', function (data) {
+    this.recvMessage(data.nickname + " has disconnected.");
+    this.removeUser(data.nickname);
+  }.bind(this));
+  
+  socket.on('userChangeName', function (data) {
+    this.recvMessage(data.oldNickname + " is now " + data.newNickname + ".");
+    this.updateUser(data.oldNickname, data.newNickname);
+  }.bind(this));
+
   socket.on('message', function (data) {
     this.recvMessage(data.text);
   }.bind(this));
+  
+  socket.on('nicknameChangeResult', function (data) {
+    console.log(data);
+  }.bind(this));
 };
 
-ChatUI.prototype.getMessage = function () {
+ChatUI.prototype.getInputValue = function () {
   return this.$input.val();
 };
 
-ChatUI.prototype.sendMessage = function (message) {
-  this.chat.sendMessage(message);
-  this.resetForm();
+ChatUI.prototype.recvMessage = function (message) {
+  this.$messages.prepend('<li>' + message + '</li>'); 
 };
 
-ChatUI.prototype.recvMessage = function (message) {
-  this.$messages.append('<li>' + message + '</li>'); 
+ChatUI.prototype.addUser = function (user) {
+  var $user = this.findUser(user);
+  
+  if (!$user) {
+    this.$users.append('<li>' + user + '</li>'); 
+  }
 };
+
+ChatUI.prototype.removeUser = function (user) {
+  var $user = this.findUser(user);
+  
+  if ($user) {
+    $user.remove();
+  }
+};
+
+ChatUI.prototype.updateUser = function (oldNickname, newNickname) {
+  var $user = this.findUser(oldNickname);
+  
+  if ($user) {
+    $user.text(newNickname);
+  }
+};
+
+ChatUI.prototype.findUser = function (user) {
+  var $user = null;
+  
+  this.$users.children("li").each(function () {    
+    var $el = $(this);
+    
+    if ($el.text() === user) {
+      $user = $el;
+      return;
+    }
+  });
+  
+  return $user
+}
 
 ChatUI.prototype.resetForm = function () {
   this.$input.val('');
