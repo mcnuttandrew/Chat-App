5ef41c32e6c39ae7cf4a41e5bd3b61ddc81d0b82
App Academy Student <student@appacademy.io>
2014-09-22 11:59:24 -0700

    refactored index.html into chat_ui.js

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -1,19 +1,49 @@
 var socket = io();
 
-function ChatUI($input, $messages){
+function ChatUI($form, $input, $messages) {
+  this.running = false;
+  this.$form = $form;
   this.$input = $input;
   this.$messages = $messages;
   this.chat = new Chat(socket);
 }
 
-ChatUI.prototype.getMessage = function(){
+ChatUI.prototype.run = function () {
+  if (this.running) {
+    console.log('ChatUI is already running!');
+  } else {
+    this.bindEvents();
+    this.$input.focus();
+  }
+};
+
+ChatUI.prototype.bindEvents = function () {
+  this.$form.on('submit', function (event) {
+    event.preventDefault();
+    
+    var message = this.getMessage();
+    this.sendMessage(message);
+  }.bind(this));
+  
+  socket.on('message', function (data) {
+    this.recvMessage(data.text);
+  }.bind(this));
+};
+
+ChatUI.prototype.getMessage = function () {
   return this.$input.val();
 };
 
-ChatUI.prototype.sendMessage = function(message) {
+ChatUI.prototype.sendMessage = function (message) {
   this.chat.sendMessage(message);
+  this.resetForm();
 };
 
-ChatUI.prototype.recvMessage = function(message){
+ChatUI.prototype.recvMessage = function (message) {
   this.$messages.append('<li>' + message + '</li>'); 
+};
+
+ChatUI.prototype.resetForm = function () {
+  this.$input.val('');
+  this.$input.focus();
 };
